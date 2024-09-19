23916aa2bf4b4265e9ced9c40a10c861f418d5ff
tahmina-nishu <akthertahmina572@gmail.com>
2024-09-19 13:58:44 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,16 @@
+const eventt = document.getElementById('add_event');
+eventt.style.backgroundColor = "lavender";
+eventt.style.borderRadius = "10px";
+eventt.style.padding = "10px";
+eventt.style.margin = "10px";
+
+const butns = document.querySelectorAll('button');
+for(butn of butns)
+{
+    butn.style.padding = "10px";
+    butn.style.width = "150px";
+    butn.style.margin = "10px";
+    butn.style.backgroundColor = "LavenderBlush";
+    butn.style.border = "2px solid RosyBrown";
+    butn.style.fontSize = "14px";
+}
